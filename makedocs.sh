80f0d9e2c51ed5e1eaf853250ac95c2efa28831f
Per Nordlöw <per.nordlow@gmail.com>
2023-11-07 11:08:33 +0100

    In doc generation, pass --case-insensitive-filenames and correct cleanup
    Also Improve messages and add extra newlines to make script structure
    a bit more clear

--- makedocs.sh
+++ makedocs.sh
@@ -5,21 +5,30 @@ set -euo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_DIR=$SCRIPT_DIR
 SOURCE_DIR=${PROJECT_DIR}/source/nanomsg
-echo generating documents for "${PROJECT_DIR}"
+
+echo Generating documentation for "${PROJECT_DIR}" into directory docs ...
+
 mkdir -p docs
-[ -e tmp ] && rm -r tmp
+
+[ -e tmp ] && rm -rf tmp
 mkdir tmp
 cd tmp
 git clone https://github.com/adamdruppe/adrdox
 cp "${PROJECT_DIR}"/.skeleton.html adrdox/skeleton.html
 mkdir -p "${PROJECT_DIR}"/docs/examples
+
 cd adrdox
 make
-./doc2 -i "${SOURCE_DIR}"
+
+./doc2 --case-insensitive-filenames -i "${SOURCE_DIR}"
 mv generated-docs/* "${PROJECT_DIR}"/docs
-./doc2 -i "${PROJECT_DIR}"/examples
+
+./doc2 --case-insensitive-filenames -i "${PROJECT_DIR}"/examples
 mv generated-docs/* "${PROJECT_DIR}"/docs/examples
+
 cp "${PROJECT_DIR}"/docs/nanomsg.html "${PROJECT_DIR}"/docs/index.html
 cd "${PROJECT_DIR}"
-rm -r tmp
-echo succeeded - docs generated
+
+rm -rf tmp
+
+echo Generation of documentation for "${PROJECT_DIR}" succeeded
